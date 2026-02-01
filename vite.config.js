c26f03a18fc6e385b5eeb1ed5f3940775543a574
emilioexperience <x4n9f6f7xg@privaterelay.appleid.com>
2026-02-01 14:47:29 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/beervault/', // Change this to your repo name
+     base: '/',  // Change this to your repo name
   server: {
     port: 3000,
     open: true
